5f95e720779c401469b5ac7ddd8f92c0a07b124d
Christian Rich <christian.schlosrich@gmail.com>
2019-07-06 13:07:59 +1000

    del

--- bin/www.js
+++
@@ -1,6 +0,0 @@
-// import app from '../src'
-// ;(async () => {
-//   app.server.listen(process.env.PORT || 3000, () =>
-//     console.log(`Express server started on port ${app.server.address().port}`)
-//   )
-// })()
